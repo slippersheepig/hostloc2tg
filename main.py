3a013c295ca4d5ba1ce181258f456a1cf76aa350
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-11-16 18:14:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ CHANNEL_ID = config["CHANNEL_ID"]
 # 关键字过滤
 KEYWORDS_WHITELIST = config.get("KEYWORDS_WHITELIST").split(',') if config.get("KEYWORDS_WHITELIST") else []
 KEYWORDS_BLACKLIST = config.get("KEYWORDS_BLACKLIST").split(',') if config.get("KEYWORDS_BLACKLIST") else []
+# 发帖人屏蔽名单
+BLOCKED_POSTERS = config.get("BLOCKED_POSTERS").split(',') if config.get("BLOCKED_POSTERS") else []
 
 # 上次检查的时间戳，初始设为当前时间 - 3分钟
 last_check = int(time.time()) - 180
@@ -53,16 +55,16 @@ async def check_hostloc():
     for link in reversed(post_links):  # 遍历最新的帖子链接，从后往前
         post_link = "https://www.hostloc.com/" + link['href']
         post_title = link.string
+        post_poster = link.parent.find_previous('a').string
 
         # 获取帖子发布时间
         post_time_str = link.parent.find_next('em').text
         post_time = parse_relative_time(post_time_str)
 
-        # 如果没有指定关键字或帖子链接不在已推送过的新贴集合中，
+        # 如果没有发布人屏蔽，且没有指定关键字或帖子链接不在已推送过的新贴集合中，
         # 并且发布时间在上次检查时间之后，并且标题包含白名单关键字，
-        # 并且标题不包含黑名单关键字，
-        # 发送到Telegram Channel并将链接加入已推送集合
-        if post_link not in pushed_posts and post_time is not None and post_time > last_check:
+        # 并且标题不包含黑名单关键字，发送到Telegram Channel并将链接加入已推送集合
+        if post_poster not in BLOCKED_POSTERS and post_link not in pushed_posts and post_time is not None and post_time > last_check:
             if (not KEYWORDS_WHITELIST or any(keyword in post_title for keyword in KEYWORDS_WHITELIST)) and not any(keyword in post_title for keyword in KEYWORDS_BLACKLIST):
                 pushed_posts.add(post_link)
                 await send_message(f"{post_title}\n{post_link}")
