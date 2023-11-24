0499975a5f79723f197943f2be301f1b2422907f
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-11-24 23:29:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import asyncio
 import telegram
 from dotenv import dotenv_values
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 
 # 从.env文件中读取配置
 config = dotenv_values("/opt/h2tg/.env")
@@ -14,17 +15,17 @@ BOT_TOKEN = config["BOT_TOKEN"]
 # Telegram Channel 的 ID
 CHANNEL_ID = config["CHANNEL_ID"]
 # 关键字过滤
-KEYWORDS_WHITELIST = config.get("KEYWORDS_WHITELIST", "").split(',')
-KEYWORDS_BLACKLIST = config.get("KEYWORDS_BLACKLIST", "").split(',')
+KEYWORDS_WHITELIST = config.get("KEYWORDS_WHITELIST").split(',') if config.get("KEYWORDS_WHITELIST") else []
+KEYWORDS_BLACKLIST = config.get("KEYWORDS_BLACKLIST").split(',') if config.get("KEYWORDS_BLACKLIST") else []
 # 发帖人屏蔽名单
-BLOCKED_POSTERS = config.get("BLOCKED_POSTERS", "").split(',')
+BLOCKED_POSTERS = config.get("BLOCKED_POSTERS").split(',') if config.get("BLOCKED_POSTERS") else []
 
 # 上次检查的时间戳，初始设为当前时间 - 3分钟
 last_check = int(time.time()) - 180
 # 保存已推送过的新贴链接
 pushed_posts = set()
 
-# 发送消息到Telegram Channel
+# 发送消息到 Telegram Channel
 async def send_message(msg):
     bot = telegram.Bot(token=BOT_TOKEN)
     await bot.send_message(chat_id=CHANNEL_ID, text=msg)
@@ -36,15 +37,19 @@ def parse_relative_time(relative_time_str):
     else:
         return None
 
-# 检查hostloc.com的新贴子
+# 检查 hostloc.com 的新贴子
 async def check_hostloc():
     global last_check
+    # 生成随机User-Agent和Cookie
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    cookie = f'hostloc_session={random.randint(100000, 999999)}'
 
-    # 使用手机版UA访问hostloc.com
+    # 对hostloc.com发起请求，获取最新的帖子链接和标题
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; Pixel 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Mobile Safari/537.36'
+        'User-Agent': user_agent,
+        'Cookie': cookie
     }
-    response = requests.get("https://www.hostloc.com/forum.php?mobile=2&mod=guide&view=newthread", headers=headers)
+    response = requests.get("https://www.hostloc.com/forum.php?mod=guide&view=newthread", headers=headers)
     html_content = response.text
 
     # 解析HTML内容，提取最新的帖子链接和标题
@@ -52,7 +57,7 @@ async def check_hostloc():
     post_links = soup.select(".xst")
 
     # 遍历最新的帖子链接
-    for link in reversed(post_links):
+    for link in reversed(post_links):  # 遍历最新的帖子链接，从后往前
         post_link = "https://www.hostloc.com/" + link['href']
         post_title = link.string
         post_poster = link.parent.find_previous('a').string
@@ -61,10 +66,11 @@ async def check_hostloc():
         post_time_str = link.parent.find_next('em').text
         post_time = parse_relative_time(post_time_str)
 
-        # 如果没有在发帖人屏蔽名单中，并且帖子链接不在已推送过的新贴集合中，并且发布时间在上次检查时间之后
-        # 并且标题包含白名单关键字，并且标题不包含黑名单关键字，发送到Telegram Channel并将链接加入已推送集合
+        # 如果没有发布人屏蔽，且没有指定关键字或帖子链接不在已推送过的新贴集合中，
+        # 并且发布时间在上次检查时间之后，并且标题包含白名单关键字，
+        # 并且标题不包含黑名单关键字，发送到Telegram Channel并将链接加入已推送集合
         if post_poster not in BLOCKED_POSTERS and post_link not in pushed_posts and post_time is not None and post_time > last_check:
-            if (not KEYWORDS_WHITELIST or any(keyword in post_title for keyword in KEYWORDS_WHITELIST)) and not any(keyword in post_title for keyword in KEYWORDS_BLACKLIST):
+            if not KEYWORDS_WHITELIST or any(keyword in post_title for keyword in KEYWORDS_WHITELIST) and not any(keyword in post_title for keyword in KEYWORDS_BLACKLIST):
                 pushed_posts.add(post_link)
                 await send_message(f"{post_title}\n{post_link}")
 
