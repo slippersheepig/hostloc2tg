c9ad16fc95c9ab1f835e12b6980352f1d38f383e
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-11-24 21:16:50 +0800

    try to fix

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from dotenv import dotenv_values
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 
+
 # 从.env文件中读取配置
 config = dotenv_values("/opt/h2tg/.env")
 
@@ -25,10 +26,12 @@ last_check = int(time.time()) - 180
 # 保存已推送过的新贴链接
 pushed_posts = set()
 
+
 # 发送消息到 Telegram Channel
 async def send_message(msg):
     bot = telegram.Bot(token=BOT_TOKEN)
     await bot.send_message(chat_id=CHANNEL_ID, text=msg)
+
 
 def parse_relative_time(relative_time_str):
     if "分钟前" in relative_time_str:
@@ -37,6 +40,7 @@ def parse_relative_time(relative_time_str):
     else:
         return None
 
+
 # 检查 hostloc.com 的新贴子
 async def check_hostloc():
     global last_check
@@ -44,7 +48,20 @@ async def check_hostloc():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
-    response = requests.get("https://www.hostloc.com/forum.php?mod=guide&view=newthread", headers=headers)
+    session = requests.Session()
+    response = session.get("https://www.hostloc.com/forum.php?mod=guide&view=newthread", headers=headers)
+    cookie = response.headers['Set-Cookie']
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # 获取JS验证的参数和URL
+    js_param = soup.select_one('[name="jscpqform:parameters"]')['value']
+    js_url = 'https://www.hostloc.com/' + soup.select_one('[id="jscpqform"]')['action']
+
+    # 发送POST请求进行JS验证
+    session.headers = headers
+    session.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    session.headers['Cookie'] = cookie
+    response = session.post(js_url, headers=headers, data={'jscpqform:parameters': js_param})
     html_content = response.text
 
     # 解析HTML内容，提取最新的帖子链接和标题
@@ -64,21 +81,29 @@ async def check_hostloc():
         # 如果没有发布人屏蔽，且没有指定关键字或帖子链接不在已推送过的新贴集合中，
         # 并且发布时间在上次检查时间之后，并且标题包含白名单关键字，
         # 并且标题不包含黑名单关键字，发送到Telegram Channel并将链接加入已推送集合
-        if post_poster not in BLOCKED_POSTERS and post_link not in pushed_posts and post_time is not None and post_time > last_check:
-            if (not KEYWORDS_WHITELIST or any(keyword in post_title for keyword in KEYWORDS_WHITELIST)) and not any(keyword in post_title for keyword in KEYWORDS_BLACKLIST):
-                pushed_posts.add(post_link)
-                await send_message(f"{post_title}\n{post_link}")
+        if (
+            post_poster not in BLOCKED_POSTERS
+            and post_link not in pushed_posts
+            and post_time is not None
+            and post_time > last_check
+            and (not KEYWORDS_WHITELIST or any(keyword in post_title for keyword in KEYWORDS_WHITELIST))
+            and not any(keyword in post_title for keyword in KEYWORDS_BLACKLIST)
+        ):
+            pushed_posts.add(post_link)
+            await send_message(f"{post_title}\n{post_link}")
 
     # 更新上次检查的时间为最后一个帖子的发布时间
     if post_links and post_time is not None:
         last_check = post_time
+
 
 # 使用 asyncio.create_task() 来运行 check_hostloc() 作为异步任务
 async def run_scheduler():
     # 每隔1-2分钟执行一次检查
     while True:
         await asyncio.sleep(random.uniform(60, 120))
-        asyncio.create_task(check_hostloc())
+        await asyncio.create_task(check_hostloc())
+
 
 # 启动定时任务
 if __name__ == "__main__":
