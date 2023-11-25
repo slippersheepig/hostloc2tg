19290e57f9c7dcf83a04fc5cd09c17c880e7ad49
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-11-25 17:08:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -40,38 +40,43 @@ def parse_relative_time(relative_time_str):
 # 检查 hostloc.com 的新贴子
 async def check_hostloc():
     global last_check
-    # 对hostloc.com发起请求，获取最新的帖子链接和标题
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; Pixel 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Mobile Safari/537.36'
-    }
-    response = requests.get("https://www.hostloc.com/forum.php?mobile=yes&mod=guide&view=newthread", headers=headers)
-    html_content = response.text
+    try:
+        # 发送请求，获取最新的帖子链接和标题
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+        response = requests.get("https://www.hostloc.com/forum.php?mod=guide&view=newthread", headers=headers)
+        response.raise_for_status()  # 检查请求是否成功
+        html_content = response.text
 
-    # 解析HTML内容，提取最新的帖子链接和标题
-    soup = BeautifulSoup(html_content, 'html.parser')
-    post_links = soup.select(".xst")
+        # 解析HTML内容，提取最新的帖子链接和标题
+        soup = BeautifulSoup(html_content, 'html.parser')
+        post_links = soup.select(".xst")
 
-    # 遍历最新的帖子链接
-    for link in reversed(post_links):  # 遍历最新的帖子链接，从后往前
-        post_link = "https://www.hostloc.com/" + link['href']
-        post_title = link.string
-        post_poster = link.parent.find_previous('a').string
+        # 遍历最新的帖子链接
+        for link in reversed(post_links):  # 遍历最新的帖子链接，从后往前
+            post_link = "https://www.hostloc.com/" + link['href']
+            post_title = link.string
+            post_poster = link.parent.find_previous('a').string
 
-        # 获取帖子发布时间
-        post_time_str = link.parent.find_next('em').text
-        post_time = parse_relative_time(post_time_str)
+            # 获取帖子发布时间
+            post_time_str = link.parent.find_next('em').text
+            post_time = parse_relative_time(post_time_str)
 
-        # 如果没有发布人屏蔽，且没有指定关键字或帖子链接不在已推送过的新贴集合中，
-        # 并且发布时间在上次检查时间之后，并且标题包含白名单关键字，
-        # 并且标题不包含黑名单关键字，发送到Telegram Channel并将链接加入已推送集合
-        if post_poster not in BLOCKED_POSTERS and post_link not in pushed_posts and post_time is not None and post_time > last_check:
-            if (not KEYWORDS_WHITELIST or any(keyword in post_title for keyword in KEYWORDS_WHITELIST)) and not any(keyword in post_title for keyword in KEYWORDS_BLACKLIST):
-                pushed_posts.add(post_link)
-                await send_message(f"{post_title}\n{post_link}")
+            # 如果没有发布人屏蔽，且没有指定关键字或帖子链接不在已推送过的新贴集合中，
+            # 并且发布时间在上次检查时间之后，并且标题包含白名单关键字，
+            # 并且标题不包含黑名单关键字，发送到Telegram Channel并将链接加入已推送集合
+            if post_poster not in BLOCKED_POSTERS and post_link not in pushed_posts and post_time is not None and post_time > last_check:
+                if (not KEYWORDS_WHITELIST or any(keyword in post_title for keyword in KEYWORDS_WHITELIST)) and not any(keyword in post_title for keyword in KEYWORDS_BLACKLIST):
+                    pushed_posts.add(post_link)
+                    await send_message(f"{post_title}\n{post_link}")
 
-    # 更新上次检查的时间为最后一个帖子的发布时间
-    if post_links and post_time is not None:
-        last_check = post_time
+        # 更新上次检查的时间为最后一个帖子的发布时间
+        if post_links and post_time is not None:
+            last_check = post_time
+            
+    except (requests.RequestException, ValueError, KeyError) as e:
+        print(f"Error occurred: {e}")
 
 # 使用 asyncio.create_task() 来运行 check_hostloc() 作为异步任务
 async def run_scheduler():
