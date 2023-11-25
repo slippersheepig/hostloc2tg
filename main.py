ebc35bc19150f1a48a954ba1d4c05ebfc66a34c6
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-11-25 10:23:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,62 +6,6 @@ import telegram
 from dotenv import dotenv_values
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
-import textwrap
-import re
-from pyaes import AESModeOfOperationCBC
-
-# 使用Python实现防CC验证页面中JS写的的toNumbers函数
-def toNumbers(secret: str) -> list:
-    text = []
-    for value in textwrap.wrap(secret, 2):
-        text.append(int(value, 16))
-    return text
-
-# 不带Cookies访问论坛首页，检查是否开启了防CC机制，将开启状态、AES计算所需的参数全部放在一个字典中返回
-def check_anti_cc() -> dict:
-    result_dict = {}
-    headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
-    home_page = "https://hostloc.com/forum.php"
-    res = requests.get(home_page, headers=headers)
-    aes_keys = re.findall(r'toNumbers\("(.*?)"\)', res.text)
-    cookie_name = re.findall('cookie="(.*?)="', res.text)
-
-    if len(aes_keys) != 0:  # 开启了防CC机制
-        print("检测到防 CC 机制开启！")
-        if len(aes_keys) != 3 or len(cookie_name) != 1:  # 正则表达式匹配到了参数，但是参数个数不对（不正常的情况）
-            result_dict["ok"] = 0
-        else:  # 匹配正常时将参数存到result_dict中
-            result_dict["ok"] = 1
-            result_dict["cookie_name"] = cookie_name[0]
-            result_dict["a"] = aes_keys[0]
-            result_dict["b"] = aes_keys[1]
-            result_dict["c"] = aes_keys[2]
-    else:
-        pass
-    return result_dict
-
-# 在开启了防CC机制时使用获取到的数据进行AES解密计算生成一条Cookie（未开启防CC机制时返回空Cookies）
-def gen_anti_cc_cookies() -> dict:
-    cookies = {}
-    anti_cc_status = check_anti_cc()
-
-    if anti_cc_status:  # 不为空，代表开启了防CC机制
-        if anti_cc_status["ok"] == 0:
-            print("防 CC 验证过程所需参数不符合要求，页面可能存在错误！")
-        else:  # 使用获取到的三个值进行AES Cipher-Block Chaining解密计算以生成特定的Cookie值用于通过防CC验证
-            print("自动模拟计尝试通过防 CC 验证")
-            a = bytes(toNumbers(anti_cc_status["a"]))
-            b = bytes(toNumbers(anti_cc_status["b"]))
-            c = bytes(toNumbers(anti_cc_status["c"]))
-            cbc_mode = AESModeOfOperationCBC(a, b)
-            result = cbc_mode.decrypt(c)
-            name = anti_cc_status["cookie_name"]
-            cookies[name] = result.hex()
-    else:
-        pass
-    return cookies
 
 # 从.env文件中读取配置
 config = dotenv_values("/opt/h2tg/.env")
@@ -81,9 +25,29 @@ last_check = int(time.time()) - 180
 # 保存已推送过的新贴链接
 pushed_posts = set()
 
+# 登录hostloc.com账号
+def login_hostloc():
+    # 使用requests库发送登录请求
+    login_data = {
+        "username": config["HOSTLOC_USERNAME"],
+        "password": config["HOSTLOC_PASSWORD"],
+        "fastloginfield": "login",
+        "quickforward": "yes",
+        "handlekey": "ls",
+        "username": config["HOSTLOC_USERNAME"],
+        "password": config["HOSTLOC_PASSWORD"]
+    }
+    response = requests.post("https://www.hostloc.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1", data=login_data)
+
+    # 检查登录是否成功
+    if "您已经顺利登录" in response.text:
+        return True
+    else:
+        return False
+
 # 发送消息到 Telegram Channel
 async def send_message(msg):
-    bot= telegram.Bot(token=BOT_TOKEN)
+    bot = telegram.Bot(token=BOT_TOKEN)
     await bot.send_message(chat_id=CHANNEL_ID, text=msg)
 
 def parse_relative_time(relative_time_str):
@@ -100,35 +64,37 @@ async def check_hostloc():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
-    cookies = gen_anti_cc_cookies()  # 获取生成的Cookie
-    response = requests.get("https://www.hostloc.com/forum.php?mod=guide&view=newthread", headers=headers, cookies=cookies)
-    html_content = response.text
 
-    # 解析HTML内容，提取最新的帖子链接和标题
-    soup = BeautifulSoup(html_content, 'html.parser')
-    post_links = soup.select(".xst")
+    # 登录hostloc.com账号
+    if login_hostloc():
+        response = requests.get("https://www.hostloc.com/forum.php?mod=guide&view=newthread", headers=headers)
+        html_content = response.text
 
-    # 遍历最新的帖子链接
-    for link in reversed(post_links):  # 遍历最新的帖子链接，从后往前
-        post_link = "https://www.hostloc.com/" + link['href']
-        post_title = link.string
-        post_poster = link.parent.find_previous('a').string
+        # 解析HTML内容，提取最新的帖子链接和标题
+        soup = BeautifulSoup(html_content, 'html.parser')
+        post_links = soup.select(".xst")
 
-        # 获取帖子发布时间
-        post_time_str = link.parent.find_next('em').text
-        post_time = parse_relative_time(post_time_str)
+        # 遍历最新的帖子链接
+        for link in reversed(post_links):  # 遍历最新的帖子链接，从后往前
+            post_link = "https://www.hostloc.com/" + link['href']
+            post_title = link.string
+            post_poster = link.parent.find_previous('a').string
 
-        # 如果没有发布人屏蔽，且没有指定关键字或帖子链接不在已推送过的新贴集合中，
-        # 并且发布时间在上次检查时间之后，并且标题包含白名单关键字，
-        # 并且标题不包含黑名单关键字，发送到Telegram Channel并将链接加入已推送集合
-        if post_poster not in BLOCKED_POSTERS and post_link not in pushed_posts and post_time is not None and post_time > last_check:
-            if (not KEYWORDS_WHITELIST or any(keyword in post_title for keyword in KEYWORDS_WHITELIST)) and not any(keyword in post_title for keyword in KEYWORDS_BLACKLIST):
-                pushed_posts.add(post_link)
-                await send_message(f"{post_title}\n{post_link}")
+            # 获取帖子发布时间
+            post_time_str = link.parent.find_next('em').text
+            post_time = parse_relative_time(post_time_str)
 
-    # 更新上次检查的时间为最后一个帖子的发布时间
-    if post_links and post_time is not None:
-        last_check = post_time
+            # 如果没有发布人屏蔽，且没有指定关键字或帖子链接不在已推送过的新贴集合中，
+            # 并且发布时间在上次检查时间之后，并且标题包含白名单关键字，
+            # 并且标题不包含黑名单关键字，发送到Telegram Channel并将链接加入已推送集合
+            if post_poster not in BLOCKED_POSTERS and post_link not in pushed_posts and post_time is not None and post_time > last_check:
+                if (not KEYWORDS_WHITELIST or any(keyword in post_title for keyword in KEYWORDS_WHITELIST)) and not any(keyword in post_title for keyword in KEYWORDS_BLACKLIST):
+                    pushed_posts.add(post_link)
+                    await send_message(f"{post_title}\n{post_link}")
+
+        # 更新上次检查的时间为最后一个帖子的发布时间
+        if post_links and post_time is not None:
+            last_check = post_time
 
 # 使用 asyncio.create_task() 来运行 check_hostloc() 作为异步任务
 async def run_scheduler():
