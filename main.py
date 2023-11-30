ad55470fb967e581520686273287c2c4dcc57994
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-11-30 19:40:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import time
 import random
 import asyncio
 import telegram
-import aiohttp
 from dotenv import dotenv_values
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
-from bbcode import Parser
 
 # 从.env文件中读取配置
 config = dotenv_values("/opt/h2tg/.env")
@@ -32,22 +30,33 @@ async def send_message(msg):
     bot = telegram.Bot(token=BOT_TOKEN)
     await bot.send_message(chat_id=CHANNEL_ID, text=msg, parse_mode='Markdown')
 
-async def parse_post_content(post_link):
+# 解析帖子内容（含图片、附件等）
+def parse_post_content(post_link):
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(post_link) as response:
-                response.raise_for_status()
-                html_content = await response.text()
+        response = requests.get(post_link)
+        response.raise_for_status()  # 检查请求是否成功
+        html_content = response.text
 
         soup = BeautifulSoup(html_content, 'html.parser')
         post_content_tag = soup.select_one(".t_fsz")
 
-        content = post_content_tag.get_text(strip=True) if post_content_tag else ""
-        return content
+        # 提取发帖内容
+        content = ""
+        if post_content_tag:
+            content = post_content_tag.get_text(strip=True)
+
+        # 获取图片和附件链接
+        attachments = soup.select(".pattl+.pattl")
+        images = soup.select(".pcb img")
+
+        attachment_urls = [attachment['href'] for attachment in attachments]
+        image_urls = [image['file'] for image in images]
+
+        return content, attachment_urls, image_urls
 
     except (requests.RequestException, ValueError) as e:
         print(f"发生错误: {e}")
-        return ""
+        return "", [], []
 
 def parse_relative_time(relative_time_str):
     if "分钟前" in relative_time_str:
@@ -56,70 +65,67 @@ def parse_relative_time(relative_time_str):
     else:
         return None
 
-def convert_bbcode_to_markdown(bbcode_content):
-    parser = Parser()
-    markdown_content = parser.to_md(bbcode_content)
-    return markdown_content
-
-async def check_and_send_message(post_link, post_title, post_poster, post_time):
-    global last_check
-    if (
-        post_poster not in BLOCKED_POSTERS
-        and post_link not in pushed_posts
-        and post_time is not None
-        and post_time > last_check
-    ):
-        if (
-            not KEYWORDS_WHITELIST
-            or any(keyword in post_title for keyword in KEYWORDS_WHITELIST)
-        ) and not any(keyword in post_title for keyword in KEYWORDS_BLACKLIST):
-            pushed_posts.add(post_link)
-
-            post_content = await parse_post_content(post_link)
-
-            # 转换BBCode格式为Markdown格式
-            post_content = convert_bbcode_to_markdown(post_content)
-
-            message = f"*{post_title}*\n[帖子链接]({post_link})\n{post_content}"
-
-            await send_message(message)
-
-    if post_time is not None:
-        last_check = post_time
-
+# 检查 hostloc.com 的新贴子
 async def check_hostloc():
+    global last_check
     try:
+        # 发送请求，获取最新的帖子链接和标题
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         }
+        response = requests.get("https://www.hostloc.com/forum.php?mod=guide&view=newthread", headers=headers)
+        response.raise_for_status()  # 检查请求是否成功
+        html_content = response.text
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get("https://www.hostloc.com/forum.php?mod=guide&view=newthread", headers=headers) as response:
-                response.raise_for_status()
-                html_content = await response.text()
-
+        # 解析HTML内容，提取最新的帖子链接和标题
         soup = BeautifulSoup(html_content, 'html.parser')
         post_links = soup.select(".xst")
 
-        tasks = []
+        # 遍历最新的帖子链接
         for link in reversed(post_links):
             post_link = "https://www.hostloc.com/" + link['href']
             post_title = link.string
             post_poster = link.parent.find_previous('a').string
+
+            # 获取帖子发布时间
             post_time_str = link.parent.find_next('em').text
             post_time = parse_relative_time(post_time_str)
 
-            tasks.append(check_and_send_message(post_link, post_title, post_poster, post_time))
+            # 如果没有发布人屏蔽，且没有指定关键字或帖子链接不在已推送过的新贴集合中，
+            # 并且发布时间在上次检查时间之后，发送到Telegram Channel并将链接加入已推送集合
+            if post_poster not in BLOCKED_POSTERS and post_link not in pushed_posts and post_time is not None and post_time > last_check:
+                if (not KEYWORDS_WHITELIST or any(keyword in post_title for keyword in KEYWORDS_WHITELIST)) and not any(keyword in post_title for keyword in KEYWORDS_BLACKLIST):
+                    pushed_posts.add(post_link)
 
-        await asyncio.gather(*tasks)
+                    # 解析帖子内容（含图片、附件等）
+                    post_content, attachment_urls, image_urls = parse_post_content(post_link)
+
+                    # 构建消息文本，包括帖子标题和内容
+                    message = f"*{post_title}*\n[帖子链接]({post_link})\n{post_content}"
+
+                    # 判断是否有图片或附件，如果有则添加到消息文本中
+                    if attachment_urls:
+                        message += "\n附件：" + ", ".join(attachment_urls)
+
+                    if image_urls:
+                        message += "\n图片：" + ", ".join(image_urls)
+
+                    await send_message(message)
+
+        # 更新上次检查的时间为最后一个帖子的发布时间
+        if post_links and post_time is not None:
+            last_check = post_time
 
     except (requests.RequestException, ValueError, KeyError) as e:
         print(f"发生错误: {e}")
 
+# 使用 asyncio.create_task() 来运行 check_hostloc() 作为异步任务
 async def run_scheduler():
+    # 每隔1-2分钟执行一次检查
     while True:
         await asyncio.sleep(random.uniform(60, 120))
-        await check_hostloc()
+        asyncio.create_task(check_hostloc())
 
+# 启动定时任务
 if __name__ == "__main__":
     asyncio.run(run_scheduler())
