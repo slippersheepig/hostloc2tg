18346feb58ce206cabb4373ebf5dd1dc1956b249
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2024-06-11 21:58:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,6 @@ CHANNEL_ID = config["CHANNEL_ID"]
 # 关键字过滤
 KEYWORDS_WHITELIST = config.get("KEYWORDS_WHITELIST").split(',') if config.get("KEYWORDS_WHITELIST") else []
 KEYWORDS_BLACKLIST = config.get("KEYWORDS_BLACKLIST").split(',') if config.get("KEYWORDS_BLACKLIST") else []
-# 发帖人屏蔽名单
-BLOCKED_POSTERS = config.get("BLOCKED_POSTERS").split(',') if config.get("BLOCKED_POSTERS") else []
 
 # 创建 Telegram Bot 实例
 bot = telegram.Bot(token=BOT_TOKEN)
@@ -139,7 +137,6 @@ async def check_hostloc():
         for link in reversed(post_links):
             post_link = "https://www.hostloc.com/" + link['href']
             post_title = link.string
-            post_poster = link.parent.find_previous('a').string
 
             # 获取帖子发布时间
             post_time_str = link.parent.find_next('em').text
@@ -147,7 +144,7 @@ async def check_hostloc():
 
             # 如果没有发布人屏蔽，且没有指定关键字或帖子链接不在已推送过的新贴集合中，
             # 并且发布时间在上次检查时间之后，发送到Telegram Channel并将链接加入已推送集合
-            if post_poster not in BLOCKED_POSTERS and post_link not in pushed_posts and post_time is not None and post_time > last_check:
+            if post_link not in pushed_posts and post_time is not None and post_time > last_check:
                 if (not KEYWORDS_WHITELIST or any(keyword in post_title for keyword in KEYWORDS_WHITELIST)) and not any(keyword in post_title for keyword in KEYWORDS_BLACKLIST):
                     pushed_posts.add(post_link)
 
