b8082650efebacdc795a66167d9a8fa9c93cf018
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2024-06-11 21:14:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ CHANNEL_ID = config["CHANNEL_ID"]
 # 关键字过滤
 KEYWORDS_WHITELIST = config.get("KEYWORDS_WHITELIST").split(',') if config.get("KEYWORDS_WHITELIST") else []
 KEYWORDS_BLACKLIST = config.get("KEYWORDS_BLACKLIST").split(',') if config.get("KEYWORDS_BLACKLIST") else []
-# 发帖人屏蔽名单
-BLOCKED_POSTERS = config.get("BLOCKED_POSTERS").split(',') if config.get("BLOCKED_POSTERS") else []
 
 # 创建 Telegram Bot 实例
 bot = telegram.Bot(token=BOT_TOKEN)
@@ -186,7 +184,7 @@ async def check_hostloc():
 
             # 如果没有发布人屏蔽，且没有指定关键字或帖子链接不在已推送过的新贴集合中，
             # 并且发布时间在上次检查时间之后，发送到Telegram Channel并将链接加入已推送集合
-            if post_poster not in BLOCKED_POSTERS and post_link not in pushed_posts and post_time is not None and post_time > last_check:
+            ifpost_link not in pushed_posts and post_time is not None and post_time > last_check:
                 if (not KEYWORDS_WHITELIST or any(keyword in post_title for keyword in KEYWORDS_WHITELIST)) and not any(keyword in post_title for keyword in KEYWORDS_BLACKLIST):
                     content, photo_urls, attachment_urls = parse_post_content(post_link)
                     message = f"=*{post_title}*\n{post_link}\n{clean_text(content)}"
